5254279b3b73b82d20f6725125d1d7ca5772a027
frustrabe <lukas.forner@gmail.com>
2022-04-08 19:54:33 +0200

    created Card component, display robots with dummy data

---
+++ src/robots.js
@@ -0,0 +1,20 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'Lukas Forner',
+        username: 'lux',
+        email: 'fornerl@gmail.com'
+    },
+    {
+        id: 2,
+        name: 'Ivana Stiavnicka',
+        username: 'ivanka',
+        email: 'stiav@gmail.com'
+    },
+    {
+        id: 3,
+        name: 'Pikaso Forsky',
+        username: 'piko',
+        email: 'forskyp@gmail.com'
+    },
+]
