6a3d72ea6d045df1cedf1b933f117561c1d036e9
frustrabe <lukas.forner@gmail.com>
2022-04-19 15:43:56 +0200

    further update of react libraries features

--- src/index.js
+++ src/index.js
@@ -1,16 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals.js';
 import 'tachyons';
 import App from './containers/App';
 
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-
-  , document.getElementById('root'));
+ReactDOM.createRoot(
+  document.getElementById('root'))
+  .render(<App />);
 
 reportWebVitals();
